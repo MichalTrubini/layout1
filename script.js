3d150f8bb4246d65f24eb0aa8d487baa8975613b
Michal Trubini <mchl.trubini@gmail.com>
2022-11-12 22:10:19 +0100

    slider completed

--- script.js
+++ script.js
@@ -3,19 +3,32 @@
 const productContainers = [...document.querySelectorAll('.sliderContainer')];
 const nxtBtn = [...document.querySelectorAll('.slider__arrowNext')];
 const preBtn = [...document.querySelectorAll('.slider__arrowPrev')];
+const currentItem = document.getElementById('sliderCountItem');
+const totalItems = document.getElementById('sliderCountTotal');
+
+let counter = 1
 
 productContainers.forEach((item, i) => {
 
-    let childrenCount = item.childElementCount;
-    let containerWidth = item.scrollWidth / childrenCount;
+    let style = getComputedStyle(item);
+    let flexGap = Number(style.gap.replace('px',''));
+    let containerWidth = item.children[1].offsetWidth + flexGap;
 
     nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
+        item.scrollLeft += containerWidth;  
+        if (item.classList.contains('sliderTypeOne') & counter < 7) counter +=1;
+        currentItem.innerHTML = counter;
     })
 
     preBtn[i].addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
+        if (item.classList.contains('sliderTypeOne') & counter > 1) counter -=1;
+        currentItem.innerHTML = counter;
     })
+
+    let childrenCount = item.childElementCount;
+
+    if (item.classList.contains('sliderTypeOne')) totalItems.innerHTML = childrenCount - 2;
 })
 
 //video play
