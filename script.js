208cbf0c1a146b11b9c57cedd796cc99394ca41b
Michal Trubini <mchl.trubini@gmail.com>
2022-11-13 22:07:10 +0100

    responsivity WIP

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-//slider
+//CONTENTS
+//
+//0.GENERAL
+//1.SET WIDTH OF SLIDER IMAGE (SLIDER TYPE ONE)
+//2.SET WIDTH OF SLIDER IMAGE (SLIDER TYPE TWO)
+//3.SLIDER
+//4.VIDEO
+//
+//***
+
+//0.GENERAL
+
+//variables declaration
+
+const screenWidth = screen.width;
+
+//1.SET WIDTH OF SLIDER IMAGE (SLIDER TYPE ONE)
+
+//variables declaration
+const sliderImagesTypeOne = [...document.querySelectorAll('.slider__imageTypeOne')];
+const referenceElement = document.querySelector('.blockSeven__text')
+const referenceElementWidth = referenceElement.offsetWidth;
+
+//functions
+
+sliderImagesTypeOne.forEach(item => {
+    item.style.width = referenceElementWidth + 'px'
+})
+
+//2.SET WIDTH OF SLIDER IMAGE (SLIDER TYPE TWO)
+
+//variables declaration
+
+const sliderImagesTypeTwo = [...document.querySelectorAll('.slider__imageTypeTwo')];
+const sliderTwo = document.querySelector('.sliderTypeTwo')
+const sliderTwoWidth = sliderTwo.offsetWidth
+let style = getComputedStyle(sliderTwo);
+let flexGap = Number(style.gap.replace('px',''));
+
+//functions
+
+sliderImagesTypeTwo.forEach(item => {
+    item.style.width = ((sliderTwoWidth - 2 * flexGap)/3) + 'px'
+})
+
+
+//3.SLIDER
+
+//variables declaration
 
 const productContainers = [...document.querySelectorAll('.sliderContainer')];
 const nxtBtn = [...document.querySelectorAll('.slider__arrowNext')];
 const preBtn = [...document.querySelectorAll('.slider__arrowPrev')];
+
 const currentItem = document.getElementById('sliderCountItem');
 const totalItems = document.getElementById('sliderCountTotal');
 
 let counter = 1
+
+//functions
 
 productContainers.forEach((item, i) => {
 
     let style = getComputedStyle(item);
     let flexGap = Number(style.gap.replace('px',''));
     let containerWidth = item.children[1].offsetWidth + flexGap;
+    let childrenCount = item.childElementCount;
 
     nxtBtn[i].addEventListener('click', () => {
         item.scrollLeft += containerWidth;  
@@ -26,19 +78,23 @@ productContainers.forEach((item, i) => {
         currentItem.innerHTML = counter;
     })
 
-    let childrenCount = item.childElementCount;
-
     if (item.classList.contains('sliderTypeOne')) totalItems.innerHTML = childrenCount - 2;
 })
 
-//video play
+//4.VIDEO
+
+//variables declaration
 
     const playButton = document.querySelector("#playButtonCustom");
+    const adjWidthScreen = screenWidth > 1250 ? 1250 : screen.width;
+    const videoMargin = screen.width > 950 ? 166 : 66;
+    const video = document.querySelector("#video__GF65F2")
+
+//functions
 
     playButton.addEventListener("click", () => {
-
-    const video = document.querySelector("#video__GF65F2")
-    video.play();
-    video.setAttribute('controls','true');
-    playButton.style.display = 'none'
+        video.play();
+        video.setAttribute('controls','true');
+        playButton.style.display = 'none';
+        video.style.width = adjWidthScreen - videoMargin +'px'
   });
